050de57bb1309656898379133b7a331fec397df0
jagodin <jakegod@live.ca>
2019-01-24 16:43:39 -0500

    Add files via upload

--- MMM-OCTranspo/octranspo.js
+++ MMM-OCTranspo/octranspo.js
@@ -1,70 +1,103 @@
-Module.register("octranspo",{
+Module.register('oc-test', {
+
+    scheduledTimer: -1,
 
     defaults: {
-        appID: "some appID",
-        apiID: "some apiID",
-        refreshInterval: (1000*60)/2, // refresh every 30s
-        timeFormat: "HH:mm",
-        debug: false
+        appID: '9fc072f0',
+        apiID: '5434c70b0ded082b2b35cd033ec52301',
+        refreshInterval: (1000*60)/4, // refresh every 15s
+        timeFormat: 'HH:mm',
+        debug: true,
+        busInfo: null,
+        stopNo: 3002,
+        routeNo: 61,
 
-        // TODO: Color/Format Defaults
+        routeDirection: 0 // 0 for Eastbound, 1 for Westbound
     },
 
-    getScripts: function() {
-        return ["moment.js"];
-    },
-
-    getStyles: function() {
-        return ["octranspo.css"];
-    },
-
-    getTranslations: function() {
-        return {
-            en: "translations/en.json",
-            fr: "translations/fr.json"
-        }
-    },
-
-    // Start module
     start: function() {
         Log.info("Starting module: " + this.name);
 
-        // Set locale
-        moment.locale(config.language);
+        if(!Array.isArray(this.config.busInfo)) {
+            this.config.busInfo = [];
+        }
+
+        if (this.config.debug) {
+            Log.info("Sending Notif to Node Helper.");
+        }
+
+        this.resume();
+        this.requestData();
     },
 
-    // Disable refreshing
-    suspend: function() {
-
-    },
-
-    // Enable interval refreshing
     resume: function() {
-
+        if (this.scheduledTimer === -1) {
+            if (this.config.debug) {
+                Log.info(this.name + ": Scheduling updates.");
+            }
+            var self = this;
+            this.scheduledTimer = setInterval(function () {
+                self.requestData();
+            }, this.config.refreshInterval);
+        }
     },
 
-    getDom: function () {
+    getDom: function() {
         var wrapper = document.createElement("div");
 
-        var table = document.createElement("table");
-    },
-
-    processData: function(data) {
-
+        wrapper.innerHTML = this.config.routeNo;
+        return wrapper;
     },
 
     requestData: function () {
         this.loaded = false;
         this.updateDom();
 
+        if (this.config.debug) {
+            console.log(this.name + ": Requesting data from node_helper.js");
+        }
+
         this.sendSocketNotification('GETDATA', this.config);
+    },
+
+    processData: function(payload) {
+        if (!payload) {
+            Log.error(this.name + ": Could not parse bus times.");
+            return;
+        }
+
+        if (this.config.debug) {
+            Log.info(this.name + ": Processing bus data.");
+        }
+
+        /*
+        * Data to extract
+        * routeNo - Direction
+        * scheduled time - GPS adjusted time
+        */
+
+        // Push bus data to departures array
+        // this.departures.push({
+        //     RouteNo: bus.RouteNo,
+        //     TripDestination: bus.TripDestination,
+        //     StopLabel: bus.StopLabel,
+        //     AdjustedScheduleTime: bus.AdjustedScheduleTime, // time in minutes until bus comes to stop
+        //     BusType: bus.BusType
+        // });
+
+        this.departures = [];
+
+        Log.info(payload);
     },
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === "RESPONSE") {
             this.loaded = true;
+            if (this.config.debug) {
+                Log.info("RESPONSE receieved");
+                Log.info(payload);
+            }
             this.processData(payload);
         }
     }
-
 });
--- MMM-OCTranspo/octranspo/lib/index.js
+++ MMM-OCTranspo/octranspo/lib/index.js
@@ -59,11 +59,12 @@ function getData(resource, resultName, data, callback) {
             callback(error);
         } else {
             try {
+                
                 var result = JSON.parse(body);
                 if(resultName) {
                     result = result[resultName];
                 }
-
+                
                 if(result.Error) {
                     callback(getErrorMsg(result.Error));
                 } else {
