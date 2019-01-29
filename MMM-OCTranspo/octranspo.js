e0d2942cb75c94e28c4d5daaa8cef75b0e73e13b
jagodin <jakegod@live.ca>
2019-01-29 16:04:59 -0500

    Update octranspo.js

--- MMM-OCTranspo/octranspo.js
+++ MMM-OCTranspo/octranspo.js
@@ -1,4 +1,4 @@
-Module.register('octranspo', {
+Module.register('oc-test', {
 
     scheduledTimer: -1,
 
@@ -11,7 +11,8 @@ Module.register('octranspo', {
         busInfo: null,
         stopNo: 3002,
         routeNo: 61,
-        routeDirection: null // 0 for Eastbound, 1 for Westbound, null for 1 direction
+        routeDirection: null, // 0 for Eastbound, 1 for Westbound, null for 1 direction
+        displaymode: 'default'
     },
 
     start: function() {
@@ -44,7 +45,69 @@ Module.register('octranspo', {
     getDom: function() {
         var wrapper = document.createElement("div");
 
-        wrapper.innerHTML = this.config.routeNo;
+        if (!this.loaded) {
+            wrapper.innerHTML = this.translate("LOADING");
+            return wrapper;
+        }
+
+        if (!this.departures.length) {
+            wrapper.innerHTML = this.translate("NODATA");
+            return wrapper;
+        }
+
+        var table = document.createElement("table");
+        table.id = "someTable";
+        table.className = "some class name";
+
+        if (this.config.displaymode === "default") {
+            var row = document.createElement("tr");
+            var stopHeader = document.createElement("th");
+            var scheduleHeader = document.createElement("th");
+            var gpsHeader = document.createElement("th");
+
+            scheduleHeader.innerHTML = this.translate("schedule");
+            gpsHeader.innerHTML = "GPS";
+
+            // Refactor for multiple stops
+            stopHeader.innerHTML = this.departures[0].StopLabel;
+
+            // stopHeader.className = "stop class name";
+            // scheduleHeader.className = "scheduled class name";
+            // gpsHeader.className = "gps class name";
+
+            row.appendChild(stopHeader);
+            row.appendChild(scheduleHeader);
+            row.appendChild(gpsHeader);
+
+            table.appendChild(row);
+
+
+            var row = document.createElement("tr");
+
+            // TODO: fix cells
+            for (var i in this.departures) {
+                var currentDeparture = this.departures[i];
+
+                var departureRow = document.createElement("td");
+                departureRow.innerHTML = currentDeparture.RouteNo + " - " + currentDeparture.TripDestination;
+
+                var scheduleRow = document.createElement("td");
+                if (currentDeparture.AdjustmentAge === -1) { // No GPS adjusted time found
+                    scheduleRow.innerHTML = moment().add(currentDeparture.AdjustedScheduleTime, 'minutes').format('hh:mm A');
+                } else { // GPS adjusted time found
+                    scheduleRow.innerHTML = "--";
+                }
+
+                var gpsRow = document.createElement("td");
+                if (currentDeparture.AdjustmentAge === -1) { // No GPS adjusted time found
+                    gpsRow.innerHTML = "--";
+                } else { // GPS adjusted time found
+                    scheduleRow.innerHTML = moment().add(currentDeparture.AdjustedScheduleTime, 'minutes').format('hh:mm A');
+                }
+
+            }
+        }
+
         return wrapper;
     },
 
@@ -59,8 +122,10 @@ Module.register('octranspo', {
         this.sendSocketNotification('GETDATA', this.config);
     },
 
-    processData: function(payload) {
-        if (!payload) {
+
+    // Process bus time JSON object into departures array
+    processData: function(bus) {
+        if (!bus) {
             Log.error(this.name + ": Could not parse bus times.");
             return;
         }
@@ -69,25 +134,19 @@ Module.register('octranspo', {
             Log.info(this.name + ": Processing bus data.");
         }
 
-        /*
-        * Data to extract
-        * routeNo - Direction
-        * scheduled time - GPS adjusted time
-        */
-
-        var departures = [];
-        var bus = payload;
+        this.departures = [];
 
         if (!Array.isArray(bus.Route.RouteDirection)) {
             for (var i in bus.Route.RouteDirection.Trips.Trip) {
-                departures.push({
+                this.departures.push({
                     StopNo: bus.StopNo,
                     StopLabel: bus.StopLabel,
                     RouteNo: bus.Route.RouteDirection.RouteNo,
                     TripDestination:  bus.Route.RouteDirection.Trips.Trip[i].TripDestination,
                     AdjustedScheduleTime: bus.Route.RouteDirection.Trips.Trip[i].AdjustedScheduleTime,
-                    BusType: bus.Route.RouteDirection.Trips.Trip[i].BusType
-                })
+                    BusType: bus.Route.RouteDirection.Trips.Trip[i].BusType,
+                    AdjustmentAge: bus.Route.RouteDirection.Trips.Trip[i].AdjustmentAge
+                });
             }
         } else {
             if (this.config.direction === null || this.config.direction > 1 || this.config.direction < 0) {
@@ -96,19 +155,26 @@ Module.register('octranspo', {
                 Log.error(bus.Route.RouteDirection[1].Direction + ": 1");
             } else {
                 for (var i in bus.Route.RouteDirection[this.config.direction].Trips.Trip) {
-                    departures.push({
+                    this.departures.push({
                         StopNo: bus.StopNo,
                         StopLabel: bus.StopLabel,
-                        RouteNo: bus.Route.RouteDirection.RouteNo,
-                        TripDestination:  bus.Route.RouteDirection.Trips.Trip[i].TripDestination,
-                        AdjustedScheduleTime: bus.Route.RouteDirection.Trips.Trip[i].AdjustedScheduleTime,
-                        BusType: bus.Route.RouteDirection.Trips.Trip[i].BusType
-                    })
+                        RouteNo: bus.Route.RouteDirection[this.config.direction].RouteNo,
+                        TripDestination:  bus.Route.RouteDirection[this.config.direction].Trips.Trip[i].TripDestination,
+                        AdjustedScheduleTime: bus.Route.RouteDirection[this.config.direction].Trips.Trip[i].AdjustedScheduleTime,
+                        BusType: bus.Route.RouteDirection[this.config.direction].Trips.Trip[i].BusType,
+                        AdjustmentAge: bus.Route.RouteDirection[this.config.direction].Trips.Trip[i].AdjustmentAge
+                    });
                 }
             }
         }
+
+        this.loaded = true;
+        this.updateDom();
+        Log.info(this.departures);
     },
 
+
+    // Receive data bus time JSON object from node helper
     socketNotificationReceived: function(notification, payload) {
         if (notification === "RESPONSE") {
             this.loaded = true;
